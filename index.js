0bf162b8411f06d27db41f22d426907f4c828608
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-06-21 10:23:51 -0400

    updating index.js

--- index.js
+++ index.js
@@ -1,11 +1,226 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
-const index = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const index = require('./dist/index.html')
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const index = require('./dist/index.html');
 const fs =require('fs');
 
+const promptEmployee = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your team managers name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your team managers name!');
+              return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your team managers id? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your team managers id!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your team managers email? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your team managers email!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'office',
+        message: 'What is your team managers office number? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your team managers office number!');
+            return false;
+          }
+          }
+        },   
+    ]);
+};
+
+const promptTeam = employeeData => {
+console.log(`
+=================
+Add a New Team Member
+`);
+
+// Add Additional Team Member
+if (!employeeData.team) {
+    employeeData.team = [];
+  }
+  return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your team engineers name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your team engineers name!');
+                  return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your team engineers id? (Required)',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please enter your team engineers id!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is your team engineers email? (Required)',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please enter your team engineers email!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'office',
+            message: 'What is your team engineers Github username? (Required)',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please enter your team engineers Github username!');
+                return false;
+              }
+            }
+           },
+          {
+           type: 'confirm',
+           name: 'confirmAddTeamMember',
+           message: 'Would you like to enter another team member?',
+           default: false
+        },
+        {
+        return inquirer. prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your team interns name? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                      return true;
+                    } else {
+                      console.log('Please enter your team interns name!');
+                      return false; 
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your team interns id? (Required)',
+                validate: githubInput => {
+                  if (githubInput) {
+                    return true;
+                  } else {
+                    console.log('Please enter your team interns id!');
+                    return false;
+                  }
+                }
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'What is your team interns school? (Required)',
+                validate: githubInput => {
+                  if (githubInput) {
+                    return true;
+                  } else {
+                    console.log('Please enter your team interns email!');
+                    return false;
+                  }
+                }
+              },
+              {
+                type: 'input',
+                name: 'office',
+                message: 'What is your team managers office number? (Required)',
+                validate: githubInput => {
+                  if (githubInput) {
+                    return true;
+                  } else {
+                    console.log('Please enter your team managers office number!');
+                    return false;
+                  }
+                  }
+                },   
+            ]),
+        },
+
+    ])
+    
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+// TODO: Create a function to write README file
+
+function init() {
+    return inquirer.prompt(questions)
+        .then(answers => {
+        //   const mark = generateMarkDown(answers)
+            fs.writeFile('index.html', mark, function (err) {
+              if(err) {
+                console.log('Could not generate Index.html')
+              } else {
+                console.log('Sucess: Index.html created')
+              }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+module.exports = Employee;
 
 // <!doctype html>
 // <html lang="en">
