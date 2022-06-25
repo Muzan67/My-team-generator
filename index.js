7ded2a57962eddb1b47c863f25e4341479ca2c4e
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-06-24 21:03:06 -0400

    updated js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const inquirer = require('inquirer');
+const fs =require('fs');
+
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const index = require('./dist/index.html');
+
 const fs =require('fs');
 
 const promptEmployee = () => {
@@ -212,7 +214,7 @@ function init() {
               if(err) {
                 console.log('Could not generate Index.html')
               } else {
-                console.log('Sucess: Index.html created')
+                console.log('Sucess Index.html created')
               }
             })
         })
@@ -231,6 +233,7 @@ module.exports = Employee;
 //     <title>My Team Generator</title>
 //     <link rel="stylesheet" href="./style.css">
 //     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 //   </head>
 //   <header>
 //     <div class="jumbotron">
@@ -245,7 +248,7 @@ module.exports = Employee;
 //       <div class="card-employee-card">
 //         <div class="manager-body">
 //           <div class="card text-white bg-primary mb-3">
-//             <h2><i class="fa-solid fa-mug-marshmallows"></i>Manager</h2>
+//             <h2><i class="fa-solid fa-mug-hot"></i>Manager</h2>
 //             <ul class="list-group">
 //               <li class="list-group-item">Name:</li>
 //               <li class="list-group-item">ID:</li>
@@ -288,7 +291,7 @@ module.exports = Employee;
 //         </div>
 //       </div>
 //     </div>
-    
+
 //   </row>
 
 // </main>
